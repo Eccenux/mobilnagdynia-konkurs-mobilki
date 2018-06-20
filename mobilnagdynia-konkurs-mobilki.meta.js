0591a5548d98742d8e5eaa62af9a67b188136df5
Eccenux <eccenux+git@gmail.com>
2018-06-20 02:11:23 +0200

    2.3.3 release

--- mobilnagdynia-konkurs-mobilki.meta.js
+++ mobilnagdynia-konkurs-mobilki.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name        Konkurs Rowerowy mobilki
 // @namespace   pl.enux.mobilnagdynia
-// @version     2.2.1
-// @description [2.2.1] Dostosowuje witrynę do urządzeń mobilnych (komórki itp).
+// @version     2.3.3
+// @description [2.3.3] Dostosowuje witrynę do urządzeń mobilnych (komórki itp).
 // @include     http://dopracyjaderowerem.mobilnagdynia.pl/*
 // @include     https://dopracyjaderowerem.mobilnagdynia.pl/*
 // @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
 // @run-at      document-start
 // ==/UserScript==
--- mobilnagdynia-konkurs-mobilki.user.js
+++ mobilnagdynia-konkurs-mobilki.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name		Konkurs Rowerowy mobilki
+// @name        Konkurs Rowerowy mobilki
 // @namespace   pl.enux.mobilnagdynia
-// @version	    2.3.3
+// @version     2.3.3
 // @description [2.3.3] Dostosowuje witrynę do urządzeń mobilnych (komórki itp).
-// @include	    http://dopracyjaderowerem.mobilnagdynia.pl/*
-// @include	    https://dopracyjaderowerem.mobilnagdynia.pl/*
+// @include     http://dopracyjaderowerem.mobilnagdynia.pl/*
+// @include     https://dopracyjaderowerem.mobilnagdynia.pl/*
 // @grant       GM_addStyle
 // @grant       GM_getValue
 // @grant       GM_setValue
-// @run-at	    document-start
+// @run-at      document-start
 // @updateURL   https://github.com/Eccenux/mobilnagdynia-konkurs-mobilki/raw/master/mobilnagdynia-konkurs-mobilki.meta.js
 // @downloadURL https://github.com/Eccenux/mobilnagdynia-konkurs-mobilki/raw/master/mobilnagdynia-konkurs-mobilki.user.js
 // ==/UserScript==
